d451b820364a8d74b4ea0aac16880fad249ece06
solep <solep@LENOVO-YOGA.fibertel.com.ar>
2025-10-28 23:22:48 -0300

    Interfaces = INodo e IGrafo

---
+++ Tpo/src/interfaces/IGrafo.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public interface IGrafo<T> {
+	
+		//G = {V, E} 
+		void agregarNodo(int valor); // pre: el valor no debe estar ya en el grafo
+	    void agregarArista(int origen, int destino); // pre: nodos deben existir
+	    
+	    void mostrarMatrizAdyacencia(); // Muestra la matriz de adyacencia
+	    void mostrarListaAdyacencia(); // Muestra la lista de adyacencia
+	    
+	    void bfs(int inicio); // pre: el nodo inicio debe existir
+	    void dfs(int inicio); // pre: el nodo inicio debe existir
+
+}
---
+++ Tpo/src/interfaces/INodo.java
@@ -0,0 +1,9 @@
+public interface INodoGrafo<T> {
+    T getDato();
+    void setDato(T dato);
+
+    void agregarVecino(INodoGrafo<T> nodo);
+    void eliminarVecino(INodoGrafo<T> nodo);
+
+    java.util.List<INodoGrafo<T>> getVecinos()
+}
