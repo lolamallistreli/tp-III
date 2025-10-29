524fe2d7235a1ba6b6950db86ab1ca1bb1ab6950
morenadelgado <morenadelgadofranco@gmail.com>
2025-10-29 15:06:25 -0300

    Se la clase Persona y la interface de NodoGrafo

---
+++ Tpo/src/interfaces/INodoGrafo.java
@@ -0,0 +1,39 @@
+package interfaces;
+
+import java.util.List;
+
+public interface INodoGrafo<T> {
+
+    
+    //PRE: el nodo debe estar inicializado.
+    //POST: devuelve el dato almacenado en el nodo.
+    //AXIOMA: el dato devuelto es el mismo que fue asignado por setDato().
+    
+    T getDato();
+
+    
+    //PRE: el parametro dato no debe ser nulo.
+     //POST: el nodo almacena el nuevo dato recibido.
+     //AXIOMA: despues de setDato(x), getDato() == x.
+
+    void setDato(T dato);
+
+    //PRE: el parametro nodo no debe ser nulo.
+     //POST: el nodo pasado como parametro queda agregado a la lista de vecinos, si no existia previamente.
+     // AXIOMA: si antes no existia el vecino, despues de agregarlo,getVecinos().contains(nodo) == true.
+
+    void agregarVecino(INodoGrafo<T> nodo);
+
+    // PRE: el parametro nodo debe existir como vecino.
+    //POST: el nodo queda eliminado de la lista de vecinos si estaba presente.
+    //AXIOMA: despues de eliminarlo, getVecinos().contains(nodo) == false.
+
+    void eliminarVecino(INodoGrafo<T> nodo);
+
+    
+     // PRE: el nodo debe estar inicializado.
+     // POST: devuelve una lista con todos los vecinos actuales del nodo.
+     // AXIOMA: para todo nodo vecino n en getVecinos(), n es accesible directamente desde este nodo.
+
+    List<INodoGrafo<T>> getVecinos();
+}
--- Tpo/src/modelo/Persona.java
+++ Tpo/src/modelo/Persona.java
@@ -1,5 +1,33 @@
 package modelo;
 
-public class Persona {
 
+public class Persona {
+    private int dni;
+    private String nombre;
+
+    public Persona(int dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public int getDni() { return dni; }
+    public String getNombre() { return nombre; }
+
+    @Override
+    public String toString() {
+        return "[" + dni + " - " + nombre + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persona)) return false;
+        Persona p = (Persona) o;
+        return this.dni == p.dni;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(dni);
+    }
 }
